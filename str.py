ef40bfb712b0c109c5664e392ad2a725252eb7cb
SUNDARRAJ20031214 <msundarajbe2025@gmail.com>
2025-11-24 20:47:34 +0530

    Update str.py

--- str.py
+++ str.py
@@ -1,24 +1,2 @@
-
 import streamlit as st
-
-st.title("📘 About Me")
-
-st.write(
-    """
-    ### Hi, I am a Developer 👨‍💻  
-    I love creating Streamlit websites and Machine Learning apps.
-    """
-)
-
-st.write(
-    """
-    #### Skills:
-    - Python  
-    - Machine Learning  
-    - Streamlit  
-    - Web Development  
-    """
-)
-
-st.write("---")
-st.write("Feel free to contact me for collaboration 😊")
+st.title("Hotel Sales App")
