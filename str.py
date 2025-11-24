ba1e8fee0b9bcb3fafbf6b4a8f700ab28e17a43a
SUNDARRAJ20031214 <msundarajbe2025@gmail.com>
2025-11-24 21:36:26 +0530

    Change header color from red to green

--- str.py
+++ str.py
@@ -1,6 +1,6 @@
 import streamlit as st
 st.title("Hotel Sales App Logo")
-st.markdown("<h1 style='color:red;'>Home</h1>",unsafe_allow_html=True)
+st.markdown("<h1 style='color:green;'>Home</h1>",unsafe_allow_html=True)
 tabs=st.tabs(["Home","Menu","About Us","Contact"])
 with tabs[0]:
   st.header("Raj Restaurant")
