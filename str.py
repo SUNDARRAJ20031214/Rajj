907f393e287cfecf60eae2d7d835173ccffb0214
SUNDARRAJ20031214 <msundarajbe2025@gmail.com>
2025-11-24 21:44:47 +0530

    Change font family for restaurant title

--- str.py
+++ str.py
@@ -2,5 +2,5 @@ import streamlit as st
 st.title("Hotel Sales App Logo")
 tabs=st.tabs(["Home","Menu","About Us","Contact"])
 with tabs[0]:
-  st.markdown("<h1 style='color:green;font:Times Romans;'>Raj Restaurant</h1>",unsafe_allow_html=True)
+  st.markdown("<h1 style='color:green;font-family:"Poppin",sans-serif;'>Raj Restaurant</h1>",unsafe_allow_html=True)
   
