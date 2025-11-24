bc36bf48b40e275be2700ee38e4e63f751cc35eb
SUNDARRAJ20031214 <msundarajbe2025@gmail.com>
2025-11-24 21:38:28 +0530

    Update str.py

--- str.py
+++ str.py
@@ -1,7 +1,6 @@
 import streamlit as st
 st.title("Hotel Sales App Logo")
-st.markdown("<h1 style='color:green;'>Home</h1>",unsafe_allow_html=True)
 tabs=st.tabs(["Home","Menu","About Us","Contact"])
 with tabs[0]:
-  st.header("Raj Restaurant")
+  st.markdown("<h1 style='color:green;'>Raj Restaurant</h1>",unsafe_allow_html=True)
   
