203fa21b692f1475139ce251f7e1a3d2b65c9753
SUNDARRAJ20031214 <msundarajbe2025@gmail.com>
2025-11-25 18:49:14 +0530

    Update str.py

--- str.py
+++ str.py
@@ -9,6 +9,6 @@ st.markdown("""
     """, unsafe_allow_html=True)
 tabs=st.tabs(["Home","Menu","About Us","Contact"])
 with tabs[0]:
-  st.markdown("<h1 style='color:yellow;font-family: Montserrat;'>Raj Restaurant</h1>",unsafe_allow_html=True)
+  st.markdown("<h1 style='color:red;font-family: Montserrat;'>Raj Restaurant</h1>",unsafe_allow_html=True)
   
   
